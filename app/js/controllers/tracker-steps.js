116922cc1f8882de981567a14129d9bfafa63b7a
Joe Rasmussen <stsraz@gmail.com>
2015-05-24 19:36:03 -0600

    TrackerStepsComplete
    Added directive and controller for the tracker steps.
    Added flipclock libraries and stylesheets.

--- app/js/controllers/tracker-steps.js
+++ app/js/controllers/tracker-steps.js
@@ -1,11 +1,9 @@
 (function() {
   angular.module('trackerApp')
     .controller('StepsController', function($scope) {
-      $scope.trackerSteps = trackerSteps;
-      $scope.currentName = 'Select a Tracking Object';
       $scope.step;
-      $scope.setStepName = function() {
-        $scope.currentName = $scope.trackerSteps[$scope.currentStep]['name'];
-      };
+      $scope.trackerSteps = trackerSteps;
+      $scope.currentName = '';
+      $scope.currentSubsteps = [];
     });
 })();
